8839d35b0a5514cc3628b60ef539e05351b68364
safak7 <safaksaydir@hotmail.com>
2022-06-30 02:20:11 +0300

    Core Layer

---
+++ Library.Core/Models/Book.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Library.Core.Models
+{
+    public class Book:BaseEntity
+    {
+        public int NumberOfPages { get; set; }
+        public string ISBN { get; set; }
+        public double Price { get; set; }
+        public int AuthorId { get; set; }
+        public Author Author{ get; set; }
+        public int PublisherId { get; set; }
+        public Publisher Publisher { get; set; }
+    }
+}
